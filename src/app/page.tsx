a230d3fa7ac4031ab2d03b6dd25d9b81ac656b1c
Mahfuzur Rahman Mayon <mahfujurrahmanmayon@gmail.com>
2025-05-01 06:57:53 +0600

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return ( 
+  <div className="flex min-h-screen items-center justify-center">
+    <Button variant="destructive">
+      Click me
+    </Button>
+  </div> );
+}
+ 
+export default Home;
